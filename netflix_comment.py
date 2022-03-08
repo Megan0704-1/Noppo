50dd736f2ca0e77a25d24123edbb7ad2afdd401b
ShaoXiangChien <79143079+ShaoXiangChien@users.noreply.github.com>
2022-03-08 13:25:26 +0800

    separate dcard cms

--- netflix_comment.py
+++ netflix_comment.py
@@ -6,6 +6,71 @@ import pandas as pd
 from google.cloud import firestore
 import matplotlib.pyplot as plt
 
+
+# @st.cache(suppress_st_warning=True)
+def data_loading():
+    doc_ref = df.collection('Comments').document(query)
+    doc_dt = doc_ref.get().to_dict()
+    img_url = doc_ref.get().get('img')
+    introduction = doc_ref.get().get('info')
+    scores = {'豆瓣': doc_dt['豆瓣'], 'IMDb': doc_ref.get().get(
+        'imdb'), '爛番茄': (doc_ref.get().get('rt_tm'), doc_ref.get().get('rt_aad'))}
+    print('Scores loaded')
+    date = [str(doc.get('date'))[:10]
+            for doc in doc_ref.collection('dates').stream()]
+    fb_sent = [round(doc.get('sent')*10, 2)
+               for doc in doc_ref.collection('sents').stream()]
+    print('Date and Sentiment loaded')
+    pc = []
+    nc = []
+    for doc in doc_ref.collection('pos_cms').stream():
+        cm_dt = doc.to_dict()
+        try:
+            try:
+                cm_dt['time'] = datetime.strptime(
+                    str(cm_dt['time']), '%Y-%m-%d %H:%M:%S')
+            except:
+                pass
+            cm_dt['sentiment'] = round(cm_dt['sentiment']*10, 2)
+            pc.append(cm_dt)
+        except Exception as err:
+            print(err)
+            continue
+
+    for doc in doc_ref.collection('neg_cms').stream():
+        cm_dt = doc.to_dict()
+        try:
+            try:
+                cm_dt['time'] = datetime.strptime(
+                    str(cm_dt['time']), '%Y-%m-%d %H:%M:%S')
+            except:
+                pass
+            cm_dt['sentiment'] = round(cm_dt['sentiment']*10, 2)
+            nc.append(cm_dt)
+        except Exception as err:
+            print(err)
+            continue
+    print('fb comments loaded')
+
+    dcard_sent = []
+    pos_dcard = []
+    neg_dcard = []
+    for doc in doc_ref.collection('dcard_cms').stream():
+        dt = doc.to_dict()
+        c_dt = dict()
+        c_dt['text'] = dt['text']
+        c_dt['sentiment'] = round(dt['sentiment']*10, 2)
+        if c_dt['sentiment'] >= 5:
+            pos_dcard.append(c_dt)
+        else:
+            neg_dcard.append(c_dt)
+        # dcard_comment.append(c_dt)
+        dcard_sent.append(c_dt['sentiment'])
+
+    print('dcard comments loaded')
+    return scores, img_url, introduction, date, fb_sent, pc, nc, dcard_sent, pos_dcard, neg_dcard, doc_ref, doc_dt
+
+
 st.set_option('deprecation.showPyplotGlobalUse', False)
 df = firestore.Client.from_service_account_json(
     './netflix-comment-system-firebase-adminsdk-hq5cn-9c691199bd.json')
@@ -26,61 +91,15 @@ if query not in drama_list:
     st.write('Not found')
 else:
     # Data Preparation
-    doc_ref = df.collection('Comments').document(query)
-    doc_dt = doc_ref.get().to_dict()
-    img_url = doc_ref.get().get('img')
-    introduction = doc_ref.get().get('info')
-    scores = {'豆瓣': doc_dt['豆瓣'], 'IMDb': doc_ref.get().get(
-        'imdb'), '爛番茄': (doc_ref.get().get('rt_tm'), doc_ref.get().get('rt_aad'))}
-    print('Scores loaded')
-    fb_sent = []
-    date = []
-    all_comment = []
-    for doc in doc_ref.collection('comments').stream():
-        comment_id = doc.get('comment_id')
-        if comment_id == "":
-            continue
-        cm_dt = dict()
-        comment = df.collection('comments').document(
-            str(comment_id)).get().to_dict()
-        if comment == None:
-            continue
-        try:
-            try:
-                cm_dt['time'] = datetime.strptime(
-                    str(comment['time']), '%Y-%m-%d %H:%M:%S')
-            except:
-                cm_dt['time'] = comment['time']
-            cm_dt['text'] = comment['text']
-            cm_dt['sentiment'] = round(comment['sentiment']*10, 2)
-            all_comment.append(cm_dt)
-            fb_sent.append(cm_dt['sentiment'])
-            date.append(cm_dt['time'].strftime('%Y-%m-%d'))
-        except Exception as err:
-            print(cm_dt['time'])
-            print(err)
-            continue
-    print('fb comments loaded')
-
-    dcard_sent = []
-    dcard_comment = []
-    for doc in doc_ref.collection('dcard_cms').stream():
-        dt = doc.to_dict()
-        c_dt = dict()
-        c_dt['text'] = dt['text']
-        c_dt['sentiment'] = round(dt['sentiment']*10, 2)
-        dcard_comment.append(c_dt)
-        dcard_sent.append(c_dt['sentiment'])
-
-    print('dcard comments loaded')
+    scores, img_url, introduction, date, fb_sent, pc, nc, dcard_sent, pos_dc, neg_dc, doc_ref, doc_dt = data_loading()
 
     jieba.dt.cache_file = 'jieba.cache.new'
-    all_text = ""
-    for cm in all_comment:
+    all_text = doc_dt['all_text']
+
+    for cm in pos_dc + neg_dc:
         all_text += cm['text']
 
-    for cm in dcard_comment:
-        all_text += cm['text']
+    # wordcloud
     with open('./stopwords.txt') as fh:
         stopword = [d[:-1] for d in fh.readlines()]
     docs = ' '.join([w for w in jieba.cut(all_text)
@@ -109,37 +128,31 @@ else:
             ss[i].subheader(source[i])
             ss[i].subheader(str(scores[source[i]]) + '/10')
 
-    pos_comment = [cm for cm in all_comment if 9 > cm['sentiment'] >= 5]
-    neg_comment = [cm for cm in all_comment if 2 < cm['sentiment'] < 5]
-    pos_comment.sort(key=lambda x: float(x['sentiment']), reverse=True)
-    neg_comment.sort(key=lambda x: float(x['sentiment']))
+    if len(pc) > 0 or len(nc) > 0:
+        st.header('臉書網友這樣說')
+        st.write(
+            '我們從「NETFLIX 新台灣討論區(非官方)」中找出與該劇相關的貼文並整理底下留言，透過以外送平台評論訓練的情緒分析模型來幫每則留言計算情緒分數，作為分類之依據。')
+        col1, col2 = st.columns(2)
 
-    st.header('臉書網友這樣說')
-    st.write(
-        '我們從「NETFLIX 新台灣討論區(非官方)」中找出與該劇相關的貼文並整理底下留言，透過以外送平台評論訓練的情緒分析模型來幫每則留言計算情緒分數，作為分類之依據。')
-    col1, col2 = st.columns(2)
+        col1.subheader('喜歡的人認為')
+        for c in pc:
+            sent = c['sentiment']
+            col1.write('---')
+            col1.write(c['text'])
+            col1.markdown(
+                f"<p style='text-align: right;'>情緒分數 {sent}/10</p>", unsafe_allow_html=True)
+            # col1.write(f'給分 {sent}')
+            col1.write(c['time'])
 
-    col1.subheader('喜歡的人認為')
-    pc = pos_comment[:10] if len(pos_comment) > 10 else pos_comment
-    for c in pc:
-        sent = c['sentiment']
-        col1.write('---')
-        col1.write(c['text'])
-        col1.markdown(
-            f"<p style='text-align: right;'>情緒分數 {sent}/10</p>", unsafe_allow_html=True)
-        # col1.write(f'給分 {sent}')
-        col1.write(c['time'])
-
-    col2.subheader('不喜歡的人認為')
-    nc = neg_comment[:10] if len(neg_comment) > 10 else neg_comment
-    for c in nc:
-        sent = c['sentiment']
-        col2.write('---')
-        col2.write(c['text'])
-        col2.markdown(
-            f"<p style='text-align: right;'>情緒分數 {sent}/10</p>", unsafe_allow_html=True)
-        # col2.write(f'給分 {sent}')
-        col2.write(c['time'])
+        col2.subheader('不喜歡的人認為')
+        for c in nc:
+            sent = c['sentiment']
+            col2.write('---')
+            col2.write(c['text'])
+            col2.markdown(
+                f"<p style='text-align: right;'>情緒分數 {sent}/10</p>", unsafe_allow_html=True)
+            # col2.write(f'給分 {sent}')
+            col2.write(c['time'])
 
     if len(fb_sent) > 0:
         st.subheader('臉書留言情緒分佈')
@@ -164,8 +177,8 @@ else:
         except:
             pass
 
-    pos_dc = [c for c in dcard_comment if 9.5 > c['sentiment'] > 5]
-    neg_dc = [c for c in dcard_comment if 1.5 < c['sentiment'] < 5]
+    # pos_dc = [c for c in dcard_comment if 9.5 > c['sentiment'] > 5]
+    # neg_dc = [c for c in dcard_comment if 1.5 < c['sentiment'] < 5]
     pos_dc.sort(key=lambda x: float(x['sentiment']), reverse=True)
     neg_dc.sort(key=lambda x: float(x['sentiment']))
     st.header('迪卡網友這樣說')
